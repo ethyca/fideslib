eefa9f6da8b19844c4d3efc3b709a87148d819b2
Phil Salant <PSalant@gmail.com>
2022-02-01 17:27:07 -0500

    Expose an `OAuthRouter` class
    This class is intended to be the touchpoint for external applications
    that import this library. An existing FastAPI `APIRouter` may call:
    `api_router.include_router(OAuthRouter(...))`
    in order to include the desired endpoints.

--- oauth/api/router.py
+++ oauth/api/router.py
@@ -1,155 +1,344 @@
+# pylint: disable=unused-argument
+
+from datetime import datetime
+from json import loads as load
 from logging import getLogger
-from typing import List
+from typing import Any, Callable, Generator, List, Optional
 
 from fastapi import APIRouter, Body, Depends, Request, Security, status
-from fastapi.security import HTTPBasic
+from fastapi.routing import APIRoute
+from fastapi.security import HTTPBasic, SecurityScopes
 from sqlalchemy.orm import Session
 
 import oauth.api.endpoints as endpoints
-from oauth.api.exceptions import AuthenticationException, ClientNotFoundException
+import oauth.jwt as jwt
+from oauth.api.exceptions import (
+    AuthenticationException,
+    AuthorizationException,
+    ClientNotFoundException,
+    ExpiredTokenException,
+)
 from oauth.api.models import (
     AccessToken,
     ClientCreatedResponse,
+    OAuth2ClientCredentialsBearer,
     OAuth2ClientCredentialsRequestForm,
 )
-from oauth.api.utils import validate_scopes, verify_oauth_client
+from oauth.api.utils import extract_payload, is_token_expired, validate_scopes
 from oauth.database.models import ClientDetail
 from oauth.scopes import (
     CLIENT_CREATE,
     CLIENT_DELETE,
     CLIENT_READ,
     CLIENT_UPDATE,
+    SCOPE_DOCS,
     SCOPE_READ,
     SCOPES,
 )
 
+EIGHT_DAYS = 60 * 24 * 8  # Expressed in minutes
+SORTED_SCOPES = SCOPES.sort()
+
 logger = getLogger(__name__)
-router = APIRouter(prefix=endpoints.OAUTH_PREFIX, tags=["OAuth"])
-
-
-@router.post(endpoints.TOKEN, response_model=AccessToken)
-async def acquire_access_token(
-    request: Request,
-    form_data: OAuth2ClientCredentialsRequestForm = Depends(),
-    db: Session = Depends(),
-) -> AccessToken:
-    """
-    Given a set of credentials, returns an access token.
-    """
-
-    basic_credentials = await HTTPBasic(auto_error=False)(request)
-
-    if form_data.client_id and form_data.client_secret:
-        client_id = form_data.client_id
-        client_secret = form_data.client_secret
-    elif basic_credentials:
-        client_id = basic_credentials.username
-        client_secret = basic_credentials.password
-    else:
-        raise AuthenticationException()
-
-    client_detail = ClientDetail.get(db, client_id=client_id)
-    if client_detail is None:
-        raise AuthenticationException()
-
-    if not client_detail.credentials_valid(client_secret):
-        raise AuthenticationException()
-
-    logger.info("Creating access token for client with ID '%s'", client_id)
-    return AccessToken(access_token=client_detail.create_access_code_jwe())
-
-
-@router.post(
-    endpoints.CLIENT,
-    dependencies=[Security(verify_oauth_client, scopes=[CLIENT_CREATE])],
-    response_model=ClientCreatedResponse,
-    status_code=status.HTTP_201_CREATED,
+oauth2_scheme = OAuth2ClientCredentialsBearer(
+    tokenUrl=endpoints.TOKEN,
+    scopes=SCOPE_DOCS,
 )
-def create_client(
-    *,
-    db: Session = Depends(),
-    scopes: List[str] = Body([]),
-) -> ClientCreatedResponse:
+
+
+class OAuthRouter(APIRouter):
     """
-    Creates a new client and returns the credentials.
-    """
-
-    validate_scopes(scopes)
-
-    client, secret = ClientDetail.create_client_and_secret(db, scopes)
-    logger.info("Created new client with ID '%s'", client.id)
-
-    return ClientCreatedResponse(client_id=client.id, client_secret=secret)
-
-
-@router.delete(
-    endpoints.CLIENT_BY_ID,
-    dependencies=[Security(verify_oauth_client, scopes=[CLIENT_DELETE])],
-    response_model=None,
-    status_code=status.HTTP_204_NO_CONTENT,
-)
-def delete_client(client_id: str, db: Session = Depends()) -> None:
-    """
-    Deletes the client associated with the client_id.
+    A FastAPI `APIRouter` that includes all endpoints necessary to
+    implement a complete OAuth scheme.
     """
 
-    client = ClientDetail.get(db, client_id=client_id)
-    if client is not None:
-        logger.info("Deleting client with ID '%s'", client_id)
-        client.delete(db)
+    def __init__(
+        self,
+        app_encryption_key: str,
+        db: Callable[[], Generator[Session, None, None]],
+        *,
+        oauth_access_token_expire_min: int = EIGHT_DAYS,
+        prefix: str = endpoints.OAUTH_PREFIX,
+        tags: Optional[List[str]] = None,
+    ) -> None:
+        if tags is None:
+            tags = ["OAuth"]
 
+        self.db_func = db
+        self.encryption_key = app_encryption_key
+        self.access_token_expire_min = oauth_access_token_expire_min
 
-@router.get(
-    endpoints.CLIENT_SCOPE,
-    dependencies=[Security(verify_oauth_client, scopes=[CLIENT_READ])],
-    response_model=List[str],
-)
-def get_client_scopes(client_id: str, db: Session = Depends()) -> List[str]:
-    """
-    Returns a list of the scopes associated with the client.
-    """
+        super().__init__(
+            prefix=prefix,
+            tags=tags,
+            routes=[
+                APIRoute(
+                    endpoints.TOKEN,
+                    self._acquire_access_token(),
+                    methods=["POST"],
+                    response_model=AccessToken,
+                    summary="Retrieve an access token",
+                ),
+                APIRoute(
+                    endpoints.CLIENT,
+                    self._create_client(),
+                    dependencies=[
+                        Security(
+                            self._verify_oauth_client(),
+                            scopes=[CLIENT_CREATE],
+                        ),
+                    ],
+                    methods=["POST"],
+                    response_model=ClientCreatedResponse,
+                    status_code=status.HTTP_201_CREATED,
+                    summary="Create a new client",
+                ),
+                APIRoute(
+                    endpoints.CLIENT_BY_ID,
+                    self._delete_client(),
+                    dependencies=[
+                        Security(
+                            self._verify_oauth_client(),
+                            scopes=[CLIENT_DELETE],
+                        ),
+                    ],
+                    methods=["DELETE"],
+                    response_model=None,  # Explicitly defined, to prevent overwriting
+                    status_code=status.HTTP_204_NO_CONTENT,
+                    summary="Delete a client",
+                ),
+                APIRoute(
+                    endpoints.CLIENT_SCOPE,
+                    self._get_client_scopes(),
+                    dependencies=[
+                        Security(
+                            self._verify_oauth_client(),
+                            scopes=[CLIENT_READ],
+                        ),
+                    ],
+                    methods=["GET"],
+                    response_model=List[str],
+                    summary="Retrieve the current scopes for a client",
+                ),
+                APIRoute(
+                    endpoints.CLIENT_SCOPE,
+                    self._set_client_scopes(),
+                    dependencies=[
+                        Security(
+                            self._verify_oauth_client(),
+                            scopes=[CLIENT_UPDATE],
+                        ),
+                    ],
+                    methods=["PUT"],
+                    summary="Overwrite the scopes for an existing client",
+                ),
+                APIRoute(
+                    endpoints.SCOPE,
+                    self.read_scopes(),
+                    dependencies=[
+                        Security(
+                            self._verify_oauth_client(),
+                            scopes=[SCOPE_READ],
+                        ),
+                    ],
+                    methods=["GET"],
+                    response_model=List[str],
+                    summary="Retrieve all available scopes",
+                ),
+            ],
+        )
 
-    logger.info("Fetching current permissions for client with ID '%s'", client_id)
-    client = ClientDetail.get(db, client_id=client_id)
-    return client.scopes if client is not None else []
+    async def _acquire_access_token(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Callable[..., AccessToken]:
+        """
+        Given a set of credentials, returns an access token.
+        """
 
+        async def acquire_access_token(
+            request: Request,
+            *,
+            db: Session = Depends(self.db_func),
+            form_data: OAuth2ClientCredentialsRequestForm = Depends(),
+        ) -> AccessToken:
+            basic_credentials = await HTTPBasic(auto_error=False)(request)
 
-@router.put(
-    endpoints.CLIENT_SCOPE,
-    dependencies=[Security(verify_oauth_client, scopes=[CLIENT_UPDATE])],
-    response_model=None,
-)
-def set_client_scopes(
-    client_id: str,
-    scopes: List[str],
-    db: Session = Depends(),
-) -> None:
-    """
-    Overwrites the client's scopes with those provided.
-    """
+            if form_data.client_id and form_data.client_secret:
+                client_id = form_data.client_id
+                client_secret = form_data.client_secret
+            elif basic_credentials:
+                client_id = basic_credentials.username
+                client_secret = basic_credentials.password
+            else:
+                raise AuthenticationException()
 
-    client = ClientDetail.get(db, client_id=client_id)
-    if not client:
-        raise ClientNotFoundException(client_id)
+            client_detail = ClientDetail.get(db, client_id=client_id)
+            if client_detail is None:
+                raise AuthenticationException()
 
-    validate_scopes(scopes)
+            if not client_detail.credentials_valid(client_secret):
+                raise AuthenticationException()
 
-    logger.info(
-        "Updating permissions for client with ID '%s' to: [%s]",
-        client_id,
-        ", ".join(scopes.sort()),
-    )
-    client.update(db, data={"scopes": scopes})
+            logger.info("Creating access token for client with ID '%s'", client_id)
+            return AccessToken(access_token=client_detail.create_access_code_jwe())
 
+        return acquire_access_token
 
-@router.get(
-    endpoints.SCOPE,
-    dependencies=[Security(verify_oauth_client, scopes=[SCOPE_READ])],
-    response_model=List[str],
-)
-def read_scopes() -> List[str]:
-    """
-    Returns a list of all scopes available for assignment in the system.
-    """
+    def _create_client(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Callable[..., ClientCreatedResponse]:
+        """
+        Creates a new client and returns the credentials.
+        """
 
-    return SCOPES.sort()
+        def create_client(
+            *,
+            db: Session = Depends(self.db_func),
+            scopes: List[str] = Body([]),
+        ) -> ClientCreatedResponse:
+            validate_scopes(scopes)
+
+            client, secret = ClientDetail.create_client_and_secret(db, scopes)
+            logger.info("Created new client with ID '%s'", client.id)
+
+            return ClientCreatedResponse(client_id=client.id, client_secret=secret)
+
+        return create_client
+
+    def _delete_client(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Callable[..., None]:
+        """
+        Deletes the client associated with the client_id.
+        """
+
+        def delete_client(
+            *,
+            client_id: str,
+            db: Session = Depends(self.db_func),
+        ) -> None:
+            client = ClientDetail.get(db, client_id=client_id)
+            if client is not None:
+                logger.info("Deleting client with ID '%s'", client_id)
+                client.delete(db)
+
+        return delete_client
+
+    def _get_client_scopes(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Callable[..., List[str]]:
+        """
+        Returns the list of scopes associated with a client.
+        """
+
+        def get_client_scopes(
+            *,
+            client_id: str,
+            db: Session = Depends(self.db_func),
+        ) -> List[str]:
+            logger.info(
+                "Fetching current permissions for client with ID '%s'", client_id
+            )
+            client = ClientDetail.get(db, client_id=client_id)
+            return client.scopes if client is not None else []
+
+        return get_client_scopes
+
+    @staticmethod
+    def read_scopes(*args: Any, **kwargs: Any) -> List[str]:
+        """
+        Returns a list of all scopes available for assignment.
+        """
+
+        return SORTED_SCOPES
+
+    def _set_client_scopes(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Callable[..., None]:
+        """
+        Overwrites the client's scopes with those provided.
+        """
+
+        def set_client_scopes(
+            *,
+            client_id: str,
+            scopes: List[str],
+            db: Session = Depends(self.db_func),
+        ) -> None:
+            client = ClientDetail.get(db, client_id=client_id)
+            if not client:
+                raise ClientNotFoundException(client_id)
+
+            validate_scopes(scopes)
+
+            logger.info(
+                "Updating permissions for client with ID '%s' to: [%s]",
+                client_id,
+                ", ".join(scopes.sort()),
+            )
+            client.update(db, data={"scopes": scopes})
+
+        return set_client_scopes
+
+    async def _verify_oauth_client(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Callable[..., ClientDetail]:
+        """
+        Verifies that the access token provided in the authorization header
+        contains the necessary scopes specified by the caller.
+        """
+
+        async def verify_oauth_client(
+            *,
+            authorization: str = Security(oauth2_scheme),
+            db: Session = Depends(self.db_func),
+            security_scopes: SecurityScopes,
+        ) -> ClientDetail:
+
+            if authorization is None:
+                raise AuthenticationException()
+
+            token_data = load(extract_payload(authorization, self.encryption_key))
+
+            issued_at = token_data.get(jwt.JWE_ISSUED_AT, None)
+            if not issued_at:
+                raise AuthorizationException()
+
+            if is_token_expired(
+                datetime.fromisoformat(issued_at),
+                self.access_token_expire_min,
+            ):
+                raise ExpiredTokenException()
+
+            assigned_scopes = token_data[jwt.JWE_PAYLOAD_SCOPES]
+            if not set(security_scopes.scopes).issubset(assigned_scopes):
+                raise AuthorizationException()
+
+            client_id = token_data.get(jwt.JWE_PAYLOAD_CLIENT_ID)
+            if not client_id:
+                raise AuthorizationException()
+
+            client = ClientDetail.get(db, client_id=client_id)
+            if not client:
+                raise AuthorizationException()
+
+            if not set(assigned_scopes).issubset(set(client.scopes)):
+                # If the scopes on the token are not a subset of the scopes available
+                # to the associated oauth client, this token is not valid
+                raise AuthorizationException()
+
+            return client
+
+        return verify_oauth_client
--- oauth/api/utils.py
+++ oauth/api/utils.py
@@ -1,71 +1,11 @@
 from datetime import datetime
-from json import loads as load
 from typing import List
 
-from fastapi import Security
-from fastapi.security import SecurityScopes
 from jose import jwe
 from jose.constants import ALGORITHMS
-from sqlalchemy.orm import Session
 
-from oauth.api.endpoints import TOKEN
-from oauth.api.exceptions import (
-    AuthenticationException,
-    AuthorizationException,
-    ExpiredTokenException,
-    InvalidScopeException,
-)
-from oauth.api.models import OAuth2ClientCredentialsBearer
-from oauth.database.models import ClientDetail
-import oauth.jwt as jwt
-from oauth.scopes import SCOPE_DOCS, SCOPES
-
-oauth2_scheme = OAuth2ClientCredentialsBearer(tokenUrl=TOKEN, scopes=SCOPE_DOCS)
-
-
-async def verify_oauth_client(
-    security_scopes: SecurityScopes,
-    db: Session,
-    authorization: str = Security(oauth2_scheme),
-) -> ClientDetail:
-    """
-    Verifies that the access token provided in the authorization header
-    contains the necessary scopes specified by the caller, and responds
-    with a 403 if not.
-    """
-
-    if authorization is None:
-        raise AuthenticationException()
-
-    # FIXME: extract_payload requires the security.APP_ENCRYPTION_KEY argument
-    token_data = load(extract_payload(authorization))
-
-    issued_at = token_data.get(jwt.JWE_ISSUED_AT, None)
-    if not issued_at:
-        raise AuthorizationException()
-
-    # FIXME: Requires the OAUTH_ACCESS_TOKEN_EXPIRE_MINUTES argument
-    if is_token_expired(datetime.fromisoformat(issued_at)):
-        raise ExpiredTokenException()
-
-    assigned_scopes = token_data[jwt.JWE_PAYLOAD_SCOPES]
-    if not set(security_scopes.scopes).issubset(assigned_scopes):
-        raise AuthorizationException()
-
-    client_id = token_data.get(jwt.JWE_PAYLOAD_CLIENT_ID)
-    if not client_id:
-        raise AuthorizationException()
-
-    client = ClientDetail.get(db, client_id=client_id)
-    if not client:
-        raise AuthorizationException()
-
-    if not set(assigned_scopes).issubset(set(client.scopes)):
-        # If the scopes on the token are not a subset of the scopes available
-        # to the associated oauth client, this token is not valid
-        raise AuthorizationException()
-
-    return client
+from oauth.api.exceptions import InvalidScopeException
+from oauth.scopes import SCOPES
 
 
 def extract_payload(jwe_string: str, encryption_key: str) -> str:
@@ -98,8 +38,7 @@ def generate_jwe(payload: str, encryption_key: str, encoding: str) -> str:
 
 def validate_scopes(scopes: List[str]) -> None:
     """
-    Raises an InvalidScopeException if any of the provided scopes
-    are invalid.
+    Raises an InvalidScopeException if any of the provided scopes are invalid.
     """
 
     invalid_scopes = [scope for scope in scopes if scope not in SCOPES]
