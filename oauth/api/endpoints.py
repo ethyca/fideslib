2e1ed4da347ba11161bf7dc80dc74252d7c63432
Phil Salant <PSalant@gmail.com>
2022-02-11 18:15:50 -0500

    Don't double-prefix endpoint URL paths

--- oauth/api/endpoints.py
+++ oauth/api/endpoints.py
@@ -4,9 +4,9 @@ The endpoint URL paths exposed by an OAuthRouter instance.
 
 OAUTH_PREFIX = "/oauth"
 
-CLIENT = f"{OAUTH_PREFIX}/client"
+CLIENT = "/client"
 CLIENT_BY_ID = CLIENT + "/{client_id}"
 CLIENT_SCOPE = CLIENT_BY_ID + "/scope"
 
-SCOPE = f"{OAUTH_PREFIX}/scope"
-TOKEN = f"{OAUTH_PREFIX}/token"
+SCOPE = "/scope"
+TOKEN = "/token"
