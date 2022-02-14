01a631bbba140297435edc71ad66e68950c3255f
Phil Salant <PSalant@gmail.com>
2022-02-14 16:23:07 -0500

    Better reuse of constant values

--- oauth/api/endpoints.py
+++ oauth/api/endpoints.py
@@ -4,9 +4,10 @@ The endpoint URL paths exposed by an OAuthRouter instance.
 
 OAUTH_PREFIX = "/oauth"
 
+SCOPE = "/scope"
+
 CLIENT = "/client"
 CLIENT_BY_ID = CLIENT + "/{client_id}"
-CLIENT_SCOPE = CLIENT_BY_ID + "/scope"
+CLIENT_SCOPE = CLIENT_BY_ID + SCOPE
 
-SCOPE = "/scope"
 TOKEN = "/token"
