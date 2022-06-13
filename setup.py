06b7d07484ee2711b5a2c499d909ffa787daf2e4
Paul Sanders <pau@ethyca.com>
2022-06-13 16:43:35 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires = read_requirements("requirements.txt")
 
 setup(
     name="fideslib",
-    version="2.0.3",
+    version="2.0.4",
     description="Shared libraries, for use in any fides project.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
