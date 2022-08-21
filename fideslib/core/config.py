f5d3a1b384856ae02305eafdea1d301589c3be10
Paul Sanders <pau@ethyca.com>
2022-08-20 22:54:05 -0400

    Default scopes to None

--- fideslib/core/config.py
+++ fideslib/core/config.py
@@ -17,7 +17,6 @@ from pydantic.env_settings import SettingsSourceCallable
 
 from fideslib.cryptography.cryptographic_util import generate_salt, hash_with_salt
 from fideslib.exceptions import MissingConfig
-from fideslib.oauth.scopes import SCOPES
 
 logger = logging.getLogger(__name__)
 
@@ -98,7 +97,7 @@ class SecuritySettings(FidesSettings):
     drp_jwt_secret: str
     root_username: Optional[str] = None
     root_password: Optional[str] = None
-    root_user_scopes: Optional[List[str]] = SCOPES
+    root_user_scopes: Optional[List[str]] = None
 
     @validator("app_encryption_key")
     @classmethod
